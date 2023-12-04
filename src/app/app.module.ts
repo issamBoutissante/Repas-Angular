d5decd3a734ad605a1e12439846f8cc005fcce0d
Boutissante <73436159+issamBoutissante@users.noreply.github.com>
2023-12-04 21:25:36 +0100

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Cop1Component } from './cop1/cop1.component';
-import { PostComponent } from './post/post.component';
 import { RepasComponent } from './repas/repas.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Cop1Component,
-    PostComponent,
     RepasComponent
   ],
   imports: [
